6ff6f13308ae0c1431949585c9b3380eb67fc906
phamvanlong098 <phamvanlong098@gmail.com>
2022-03-20 23:34:06 +0700

    bai thu vien

--- src/Library/Book.java
+++ src/Library/Book.java
@@ -17,6 +17,73 @@ package Library;
  *  toString(): in ra thong tin sach
  */
 public class Book {
+    private int id;
+    private String name;
+    private String author;
+    private Boolean isBorrowed;
 
+    public Book() {
+        id = 0;
+        name = "";
+        author = "";
+        isBorrowed = false;
+    }
 
+    public Book(int id, String name, String author) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        isBorrowed = false;
+    }
+
+    public Book(Book otherBook){
+        this.id = otherBook.getId();
+        this.name = otherBook.getName();
+        this.author = otherBook.getAuthor();
+        this.isBorrowed = otherBook.getIsBorrowed();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Boolean getIsBorrowed() {
+        return isBorrowed;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setIsBorrowed(Boolean isBorrowed) {
+        this.isBorrowed = isBorrowed;
+    }
+
+    public void borrow() {
+        isBorrowed = true;
+    }
+
+    public void giveBack() {
+        isBorrowed = false;
+    }
+
+    public String toString() {
+        return id + " " + name + " " + author + " " + isBorrowed;
+    }
 }
--- src/Library/Library.java
+++ src/Library/Library.java
@@ -11,4 +11,84 @@ package Library;
  *  toString(): "in ra trang thai cua thu vien"
  */
 public class Library {
+    private Book[] listBook;
+    private int numberOfBook;
+
+    public Library() {
+        listBook = new Book[100];
+        numberOfBook = 0;
+    }
+
+    public void addBook(Book book) {
+        listBook[numberOfBook] = book;
+        numberOfBook++;
+    }
+
+    public int findBook(int id) {
+        for(int i = 0; i < numberOfBook; i++) {
+            if(listBook[i].getId() == id) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public String borrowBook(int id) {
+        int index = findBook(id);
+        if(index == -1) {
+            return "Quyen sach nay khong ton tai!";
+        }
+
+        if(listBook[index].getIsBorrowed()){
+            return "Quyen sach nay da duoc muon! " + listBook[index].toString();
+        }
+        listBook[index].borrow();
+        return "Muon thanh cong! Sach cua ban: " + listBook[index].toString();
+    }
+
+    public String giveBackBook(int id) {
+        int index = findBook(id);
+        if(index == -1) {
+            return "Quyen sach nay khong ton tai!";
+        }
+
+        if(!listBook[index].getIsBorrowed()){
+            return "Chua muon thi tra kieu gi? " + listBook[index].toString();
+        }
+
+        listBook[index].giveBack();
+
+        return "Tra sach thanh cong! " + listBook[index].toString();
+    }
+
+    public String toString() {
+        String result = "\n Thong tin thu vien: \n";
+        for(int i = 0; i < numberOfBook; i++) {
+            result += i + " " + listBook[i].toString() + "\n";
+        }
+        return result;
+    }
+
+    public static void main(String args[]) {
+        Library thuVien = new Library();
+        Book book1 = new Book(101, "Lap trinh huong doi tuong", "OOP");
+        Book book2 = new Book(102, "Phan tich va thiet ke huong doi tuong", "OOAD");
+        Book book3 = new Book(103, "Lap trinh game bang Java", "MOBILE");
+
+        thuVien.addBook(book1);
+        thuVien.addBook(book2);
+        thuVien.addBook(book3);
+
+        System.out.println(thuVien.toString());
+
+        System.out.println(thuVien.borrowBook(101));
+        System.out.println(thuVien.borrowBook(102));
+        System.out.println(thuVien.borrowBook(102));
+        System.out.println(thuVien.giveBackBook(103));
+        System.out.println(thuVien.borrowBook(103));
+        System.out.println(thuVien.giveBackBook(101));
+
+
+        System.out.println(thuVien.toString());
+    }
 }
